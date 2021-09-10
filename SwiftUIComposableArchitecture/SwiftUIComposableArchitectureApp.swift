a9c7b6aa12414deb20b1098a5453a39c7d4e7441
yuoku <appledev.yuoku@gmail.com>
2021-09-10 22:16:15 +0900

    Initial Commit

---
+++ SwiftUIComposableArchitecture/SwiftUIComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComposableArchitectureApp.swift
+//  SwiftUIComposableArchitecture
+//
+//  Created by Yuki Okudera on 2021/09/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
