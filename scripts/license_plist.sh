6b1709805ad0f33dfd5076e2562a2d1b64f4c8b3
yuoku <appledev.yuoku@gmail.com>
2021-09-16 01:33:16 +0900

    Changed the file structure.

--- scripts/license_plist.sh
+++ scripts/license_plist.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-/opt/homebrew/bin/mint run mono0926/LicensePlist license-plist --output-path ${PRODUCT_NAME}/Application/Settings.bundle
+/opt/homebrew/bin/mint run mono0926/LicensePlist license-plist --output-path ${PRODUCT_NAME}/Application/Resources/Settings/Settings.bundle
